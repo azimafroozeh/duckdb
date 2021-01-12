3380acd594b39f1fa99ac659dd67d50e0fbad31e
AzimAfroozeh <afroozeh3@gmail.com>
2021-01-12 12:17:59 +0100

    add arch.h

--- examples/embedded-c++/main.cpp
+++ examples/embedded-c++/main.cpp
@@ -9,23 +9,26 @@ int main() {
     DuckDB db(nullptr);
     Connection con(db);
 
-#ifdef test
-    con.Query("CREATE TABLE test(i TINYINT NOT NULL); INSERT INTO test SELECT (i) AS i FROM range(0, 100) tbl(i);");
-#else
-    con.Query("CREATE TABLE test(i TINYINT NOT NULL); INSERT INTO test SELECT (i / 300000000) AS i FROM range(0, 300000000) tbl(i);");
-#endif
+//#ifdef test
+//    con.Query("CREATE TABLE test(i TINYINT NOT NULL); INSERT INTO test SELECT (i) AS i FROM range(0, 100) tbl(i);");
+//#else
+//    con.Query("CREATE TABLE test(i TINYINT NOT NULL); INSERT INTO test SELECT (i / 300000000) AS i FROM range(0, 1024) tbl(i);");
+//	con.Query("TRANSACTION ");
+//#endif
 
 	con.Query("PRAGMA show('table_name');");
     con.Query("PRAGMA enable_profiling;");
     auto result = con.Query("SELECT (i + i + i + i + i + i + i + i + i + i + i +"
-	                        "           i + i + i + i + i + i + i + i + i + i + i +"
-	                        "           i + i + i + i + i + i + i + i + i + i + i +"
-	                        "           i + i + i + i + i + i + i + i + i + i + i +"
-	                        "           i + i + i + i + i + i + i + i + i + i + i +"
-	                        "           i + i + i + i + i + i + i + i + i + i + i +"
-	                        "           i + i + i + i + i + i + i + i + i + i + i +"
-	                        "           i + i + i + i + i + i + i + i + i + i + i +"
-	                        "           i + i + i + i + i + i + i + i + i + i + i +"
-	                        "           i + i + i + i + i + i + i + i + i + i + i ) FROM test");
+	                        "                                   i + i + i + i + i + i + i + i + i + i + i +"
+	                        "                                   i + i + i + i + i + i + i + i + i + i + i +"
+	                        "                                   i + i + i + i + i + i + i + i + i + i + i +"
+	                        "                                   i + i + i + i + i + i + i + i + i + i + i +"
+	                        "                                   i + i + i + i + i + i + i + i + i + i + i +"
+	                        "                                   i + i + i + i + i + i + i + i + i + i + i +"
+	                        "                                   i + i + i + i + i + i + i + i + i + i + i +"
+	                        "                                   i + i + i + i + i + i + i + i + i + i + i +"
+	                        "                                   i + i + i + i + i + i + i + i + i + i + i ) FROM (SELECT CAST(j * 0 as TINYINT) AS i FROM range(0, 300000000) tbl(j)) AS T");
 
 }
+
+
