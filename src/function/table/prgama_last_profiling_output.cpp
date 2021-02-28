5259aee0ed50978351dae2da228c5a04e7383724
AzimAfroozeh <afroozeh3@gmail.com>
2021-02-28 18:58:33 +0100

    format fix

--- src/function/table/prgama_last_profiling_output.cpp
+++ src/function/table/prgama_last_profiling_output.cpp
@@ -1,28 +1,15 @@
-#include "duckdb/function/table/sqlite_functions.hpp"
-
-#include "duckdb/catalog/catalog.hpp"
-#include "duckdb/catalog/catalog_entry/table_catalog_entry.hpp"
-#include "duckdb/catalog/catalog_entry/view_catalog_entry.hpp"
-#include "duckdb/parser/qualified_name.hpp"
-#include "duckdb/planner/constraints/bound_not_null_constraint.hpp"
-#include "duckdb/planner/constraints/bound_unique_constraint.hpp"
-
-#include "duckdb/common/exception.hpp"
-#include "duckdb/common/limits.hpp"
-
-#include <algorithm>
-
 namespace duckdb {
 
 struct PragmaTableOperatorData : public FunctionOperatorData {
-	PragmaTableOperatorData(idx_t rows) : rows(rows) {
+	explicit PragmaTableOperatorData(idx_t rows) : rows(rows) {
 	}
 	idx_t rows;
 };
 
-static unique_ptr<FunctionData> bind(ClientContext &context, vector<Value> &inputs,
-                                     unordered_map<string, Value> &named_parameters, vector<LogicalType> &return_types,
-                                     vector<string> &names) {
+static unique_ptr<FunctionData> PragmaLastProfilingOutputBind(ClientContext &context, vector<Value> &inputs,
+                                                              unordered_map<string, Value> &named_parameters,
+                                                              vector<LogicalType> &return_types,
+                                                              vector<string> &names) {
 	names.emplace_back("op_id");
 	return_types.push_back(LogicalType::INTEGER);
 
@@ -38,9 +25,10 @@ static unique_ptr<FunctionData> bind(ClientContext &context, vector<Value> &inpu
 	return make_unique<TableFunctionData>();
 }
 
-unique_ptr<FunctionOperatorData> init(ClientContext &context, const FunctionData *bind_data,
-                                      vector<column_t> &column_ids, TableFilterCollection *filters) {
-	return make_unique<PragmaTableOperatorData>(1);
+unique_ptr<FunctionOperatorData> PragmaLastProfilingOutputInit(ClientContext &context, const FunctionData *bind_data,
+                                                               vector<column_t> &column_ids,
+                                                               TableFilterCollection *filters) {
+	return make_unique<PragmaTableOperatorData>(1024);
 }
 
 static void SetValue(DataChunk &output, int index, int op_id, int fun_id, string description, double time) {
@@ -50,38 +38,36 @@ static void SetValue(DataChunk &output, int index, int op_id, int fun_id, string
 	output.SetValue(3, index, time);
 }
 
-static void ExtractExpressions(ExpressionInformation &info, DataChunk &output, int *index, int op_id, int *exp_id,
+static void ExtractExpressions(ExpressionInformation &info, DataChunk &output, int &index, int op_id, int &fun_id,
                                int sample_tuples_count) {
 	if (info.hasfunction) {
-		SetValue(output, *(index), op_id, *exp_id, ", Function: " + info.function_name,
+		SetValue(output, index++, op_id, fun_id++, ", Function: " + info.function_name,
 		         double(info.time) / sample_tuples_count);
-		(*index) = *index + 1;
-		(*exp_id) = *exp_id + 1;
 	}
 	if (info.children.empty()) {
 		return;
 	}
 	// extract the children of this node
 	for (auto &child : info.children) {
-		ExtractExpressions(*child, output, index, op_id, exp_id, sample_tuples_count);
+		ExtractExpressions(*child, output, index, op_id, fun_id, sample_tuples_count);
 	}
 	return;
 }
 
-static void function1(ClientContext &context, const FunctionData *bind_data_p, FunctionOperatorData *operator_state,
-                      DataChunk &output) {
+static void PragmaLastProfilingOutputFunction(ClientContext &context, const FunctionData *bind_data_p,
+                                              FunctionOperatorData *operator_state, DataChunk &output) {
 	auto &state = (PragmaTableOperatorData &)*operator_state;
 	if (state.rows > 0) {
 		int total_counter = 0;
 		int operator_counter = 1;
 		SetValue(output, total_counter++, 0, 0, "Query: " + context.query, context.profiler.main_query.Elapsed());
 		for (auto op : context.prev_profiler.tree_map) {
-			int expression_counter = 1;
+			int function_counter = 1;
 			SetValue(output, total_counter++, operator_counter, 0, "Operator: " + op.second->name,
 			         op.second->info.time);
 			if (op.second->info.has_executor) {
 				for (auto &info : op.second->info.executors_info->roots) {
-					ExtractExpressions(*info, output, &(total_counter), operator_counter, &(expression_counter),
+					ExtractExpressions(*info, output, total_counter, operator_counter, function_counter,
 					                   op.second->info.executors_info->sample_tuples_count);
 				}
 			}
@@ -96,7 +82,8 @@ static void function1(ClientContext &context, const FunctionData *bind_data_p, F
 }
 
 void PragmaLastProfilingOutput::RegisterFunction(BuiltinFunctions &set) {
-	set.AddFunction(TableFunction("pragma_last_profiling_output", {}, function1, bind, init));
+	set.AddFunction(TableFunction("pragma_last_profiling_output", {}, PragmaLastProfilingOutputFunction,
+	                              PragmaLastProfilingOutputBind, PragmaLastProfilingOutputInit));
 }
 
 } // namespace duckdb
