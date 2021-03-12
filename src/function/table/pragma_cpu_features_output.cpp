fb7469c173ce306be5449e10fa9307bbd78916e2
AzimAfroozeh <afroozeh3@gmail.com>
2021-03-12 22:02:55 +0100

    fix

--- src/function/table/pragma_cpu_features_output.cpp
+++ src/function/table/pragma_cpu_features_output.cpp
@@ -1,11 +1,6 @@
 #include "duckdb/function/table/sqlite_functions.hpp"
-
-#include "duckdb/catalog/catalog_entry/table_catalog_entry.hpp"
-#include "duckdb/catalog/catalog_entry/view_catalog_entry.hpp"
-#include "duckdb/planner/constraints/bound_not_null_constraint.hpp"
-#include "duckdb/main/client_context.hpp"
 #include "duckdb/common/cpu_feature.hpp"
-#include "duckdb/common/limits.hpp"
+
 
 namespace duckdb {
 
